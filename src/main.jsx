4245e0c0a407d02ca94a57c8da62355c43467d69
Aleksandr3267 <sanya.pyatkov.89@mail.ru>
2024-11-07 21:25:01 +0600

    add HashRouter for main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import store from './store';
 import AuthProvider from "./auth/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // React.StrictMode
   <>
-    <Router basename="/primeholding">
+    <Router >
       <Provider store={store}>
         <AuthProvider>
             <App />
